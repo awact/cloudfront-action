3fe09fcfaf73683e6cfde0a5c4fabc103b464221
Shun Kakinoki <shunkakinoki@gmail.com>
2019-10-24 21:19:22 +0900

    added everything to source path

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ if [ -z "$AWS_REGION" ]; then
 fi
 
 # Default to invalidate the entire path if SOURCE_PATH not set.
-SOURCE_PATH=${SOURCE_PATH:.}
+SOURCE_PATH=${SOURCE_PATH:/*}
 
 # Create a dedicated profile for this action to avoid
 # conflicts with other actions.
